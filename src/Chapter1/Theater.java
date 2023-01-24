7ffa4e4270c45ef5ed82c150f286968b5f983fb4
ABlued <skwodhkswkd@naver.com>
2023-01-24 22:49:17 +0900

    comment : enter 메소드의 문제점

--- src/Chapter1/Theater.java
+++ src/Chapter1/Theater.java
@@ -8,6 +8,10 @@ public class Theater {
     }
 
     public void enter(Audience audience){
+        // enter 메소드가 하는 기능이 너무 많다.
+        // 또한 다른 클래스들의 구조를 아주 깊고 자세히 알고있어야 한다.
+        // 1. TicketSeller가 office안에서만 티켓을 받아야하는 것
+        // 2. audience가 bag을 갖고 있다는 것
         if(audience.getBag().hasInvitation()){
             Ticket ticket = ticketSeller.getTicketOffice().getTicket();
             audience.getBag().setTicket(ticket);
@@ -17,5 +21,6 @@ public class Theater {
             ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
             audience.getBag().setTicket(ticket);
         }
+        // 이렇게 다른 클래스와의 결합도가 높으면 수정하기 용이하지도 코드를 이해하기도 어렵다.
     }
 }
