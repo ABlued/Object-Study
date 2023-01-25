21e764ef5f4e468fcc3ac53e94e227100f3c7130
ABlued <skwodhkswkd@naver.com>
2023-01-25 23:20:10 +0900

    refactor : Audience의 bag 소유 사실을 숨기도록 리팩토링

--- src/Chapter1/Audience.java
+++ src/Chapter1/Audience.java
@@ -6,8 +6,16 @@ public class Audience {
     public Audience(Bag bag){
         this.bag = bag;
     }
-
-    public Bag getBag() {
-        return bag;
+    // Bag에 대한 동작을 Audience에게 직접 처리하게 하여 외부에서는
+    // 더 이상 Audience가 Bag을 소유하고 있다는 사실을 알 필요 없게하자.
+    public Long buy(Ticket ticket){
+        if(bag.hasInvitation()){
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- src/Chapter1/TicketSeller.java
+++ src/Chapter1/TicketSeller.java
@@ -6,21 +6,10 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice){
         this.ticketOffice = ticketOffice;
     }
-    // 더 이상 TicketSeller에게서 ticketoffice를 접근할 수 없도록 함
-    // 따라서 TicketSeller는 ticketOffice에서 티켓을 꺼내거나 판매 요금을 적립하는 일을 스스로 수행해야한다.
+    // 이런 식으로 Audience가 bag을 갖고 있다는 사실을 모르게 한다면
+    // Bag의 존재를 내부로 캡슐화할 수 있게 된다.
 
-    // 이처럼 객체 내부의 세부적인 사항을 감추는 것을 캡슐화라하며
-    // 캡슐화의 목적은 변경하기 쉬운 객체를 만드는 것이다.
-    // 캡슐화를 통해 객체 내부로의 접근을 제한하면 객체와 객체 사이의 결합도를 낮출 수 있기 때문에 변경하기 용이해진다.
     public void sellTo(Audience audience){
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
