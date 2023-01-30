ce1fc994583c36f5f812d3e7441f4b463705c539
ABlued <skwodhkswkd@naver.com>
2023-01-30 23:10:43 +0900

    feat : 영화의 할인 조건 PeriodCondition, SequenceCondition 클래스 구현

---
+++ src/Chapter2/SequenceCondition.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+public class SequenceCondition implements DisCountCondition{
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening){
+        return screening.isSequence(sequence);
+    }
+}
