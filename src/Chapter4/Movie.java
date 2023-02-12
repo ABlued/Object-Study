72408436deb08ffd9295e26b9d83375f3a790f0a
ABlued <skwodhkswkd@naver.com>
2023-02-12 23:34:16 +0900

    feat : Movie의 접근자, 수정자 정의

--- src/Chapter4/Movie.java
+++ src/Chapter4/Movie.java
@@ -1,16 +1,58 @@
 package Chapter4;
 
+
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 // 데이터 중심의 설계
 public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private List<DisCountCondition> disCountConditions;
+    private List<DiscountCondition> discountConditions;
 
     private MovieType movieType;
-    // discountPolicy는 없어지고 할인 정책과 관련된 할인 비율, 할인 금액이 직접 정의되어 있다.
+    // discountPolicy 없어지고 할인 정책과 관련된 할인 비율, 할인 금액이 직접 정의되어 있다.
     private Money discountAmount;
     private double discountPercent;
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }
